25a868bc618aefb70eec6fee23ab0e7623248b52
Joy Kim <wlasl0121@gmail.com>
2023-01-02 16:03:34 +0900

    DFS,BFS / 백준 1260

--- 백준/1260.js
+++ 백준/1260.js
@@ -17,6 +17,7 @@ input.reduce((prev, current, index) => {
 
 var visited = new Array(N + 1).fill(false);
 var stack = [];
+var queue = [];
 
 function dfs(v, visited) {
     stack.push(v);
@@ -30,7 +31,26 @@ function dfs(v, visited) {
     }
 }
 
-function bfs() {}
+function bfs(v) {
+    var visit = new Array(N + 1).fill(false);
+    var arr = [];
+
+    visit[v] = true;
+    arr.push(v);
+    queue.push(v);
+
+    for (var i = 0; i < N; i++) {
+        var node = arr.shift();
+        var connect = getNode(node);
+        for (var j = 0; j < connect.length; j++) {
+            if (!visit[connect[j]]) {
+                arr.push(connect[j]);
+                queue.push(connect[j]);
+                visit[connect[j]] = true;
+            }
+        }
+    }
+}
 
 function getNode(v) {
     var node = [];
@@ -41,8 +61,22 @@ function getNode(v) {
             node.push(test[0]);
         }
     }
-    return node.sort();
+    var sorting = node.sort((a, b) => {
+        return a - b;
+    });
+
+    /**
+     * 회고록 : node.sort() 를 하게 되면 1,10,11,2,3... 이런식으로 나옴
+     * 유니코드 순서로 정렬하기 때문
+     * 내가 원하는 1,2,3,...10 으로 정렬하기 위해서는 위와같이 파라미터를 사용해야한다.
+     * a-b가 음수냐, 양수냐 를 이용하여 오름차순으로 정렬할수있다.
+     */
+    //
+    //
+    return sorting;
 }
 
 dfs(V, visited);
-console.log(stack);
+console.log(stack.toString().replaceAll(",", " "));
+bfs(V);
+console.log(queue.toString().replaceAll(",", " "));
