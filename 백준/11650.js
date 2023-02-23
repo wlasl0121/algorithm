8ea8175c81fbaa2ebb495d6b7aa6944558cb187c
Joy Kim <wlasl0121@gmail.com>
2023-02-23 18:06:47 +0900

    정렬 / 백준 11650

---
+++ 백준/11650.js
@@ -0,0 +1,26 @@
+let input = require("fs")
+    .readFileSync("./백준/input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+let N = Number(input.shift());
+// console.log(input);
+
+let xy = [];
+input.map((v) => {
+    xy.push([parseInt(v.split(" ")[0]), parseInt(v.split(" ")[1])]);
+});
+// console.log(xy);
+xy.sort((a, b) => {
+    if (a[0] === b[0]) {
+        return a[1] - b[1];
+    } else {
+        return a[0] - b[0];
+    }
+});
+let answer = [];
+xy.map((v) => {
+    answer.push(`${v[0]} ${v[1]}`);
+});
+console.log(answer.join("\n"));
