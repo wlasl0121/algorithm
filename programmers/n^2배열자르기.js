f96527a28d3bfe97405a4a98edacdf65ba2c45ed
Joy Kim <wlasl0121@gmail.com>
2023-06-15 18:55:58 +0900

    구현 / 프로그래머스 n^2배열자르기

---
+++ programmers/n^2배열자르기.js
@@ -0,0 +1,12 @@
+function solution(n, left, right) {
+    var answer = [];
+
+    for (var i = left; i <= right; i++) {
+        answer.push(Math.max(Math.floor(i / n), i % n) + 1);
+    }
+
+    return answer;
+}
+
+console.log(solution(3, 2, 5));
+console.log(solution(4, 7, 14));
